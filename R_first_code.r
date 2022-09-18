1a1d5c62ceeffa13a5f2338772881c221468edb2
Cecilia Rocca <113837824+CeciliaRocca25@users.noreply.github.com>
2022-09-18 14:57:55 +0200

    Create R_first_code.r

---
+++ R_first_code.r
@@ -0,0 +1,10 @@
+# First code in R
+
+# here are the input data
+# costanza data on streams
+water<-c(100, 200, 300, 400, 500) 
+# marta data on fish genomes
+fishes<-c(10, 50, 60, 100, 200)
+# plot the diversity of fishes (y) vs the amount of water (x)
+# a function is used with arguments inside
+plot(water, fishes)
