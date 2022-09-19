994bf07dcb64e6975c04c68f7e53c4cca2535159
Cecilia Rocca <113837824+CeciliaRocca25@users.noreply.github.com>
2022-09-19 19:21:34 +0200

    Update R_first_code.r

--- R_first_code.r
+++ R_first_code.r
@@ -8,3 +8,29 @@ fishes<-c(10, 50, 60, 100, 200)
 # plot the diversity of fishes (y) vs the amount of water (x)
 # a function is used with arguments inside
 plot(water, fishes)
+#our data can be stored in a table called data frame
+streams <- data.frame(water, fishes)
+streams
+#how to import and export tables
+#set working directory for Windows
+#we use commas for anything that is outside R
+setwd("C:/lab/")
+#export table
+write.table(streams, file="my_first_table.txt")
+#import table
+read.table("my_first_table.txt")
+#we can assign this function to an object in R
+cecitable <- read.table("my_first_table.txt")
+#lazy statistics
+summary(cecitable)
+#if I only want info on fishes
+summary(cecitabletable$fishes)
+# histogram
+hist(cecitable$fishes)
+hist(cecitable$water)
+#meaning: lower amounts are more frequent than higher amounts
+
+# R code for ecosystem monitoring by remote sensing
+# we need to install raster package to manage image data
+# https://cran.r-project.org/web/packages/raster/index.html
+install.packages("raster")
