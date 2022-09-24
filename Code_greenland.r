96c7fba84338c1b1c1958b8517c30a69e4d30702
Cecilia Rocca <113837824+CeciliaRocca25@users.noreply.github.com>
2022-09-24 21:50:52 +0200

    Update Code_greenland.r

--- Code_greenland.r
+++ Code_greenland.r
@@ -21,15 +21,25 @@ cl <- colorRampPalette(c("blue","light blue","pink","yellow"))(100)
 plot(TGr, col=cl)
 
 # ggplot of first and final images 2000 vs. 2015 
-ggplot() + 
+p1 <- ggplot() + 
 geom_raster(TGr$lst_2000, mapping = aes(x=x, y=y, fill=lst_2000)) +
 scale_fill_viridis(option="magma") 
 
-ggplot() + 
-geom_raster(TGr2000, mapping = aes(x=x, y=y, fill=lst_2000)) +
+p2 <- ggplot() + 
+geom_raster(TGr$lst_2015, mapping = aes(x=x, y=y, fill=lst_2015)) +
 scale_fill_viridis(option="magma") 
 
-ggplot() + 
-geom_raster(TGr2000, mapping = aes(x=x, y=y, fill=lst_2000)) +
-scale_fill_viridis(option="viridis") +
-ggtitle("Snow cover during my birthday!")
+p1/p2
+
+# histograms
+par(mfrow=c(1,2))
+hist(TGr$lst_2000)
+hist(TGr$lst_2015)
+
+# plot two years to see where the pixels lie
+plot(TGr$lst_2000, TGr$lst_2015, xlim=c(12500, 15000), ylim=c(12500, 15000))
+abline(0,1,col="red")
+
+# plot all the graphs
+# we use the pair function to create scatterplot matrices
+pairs(TGr)
